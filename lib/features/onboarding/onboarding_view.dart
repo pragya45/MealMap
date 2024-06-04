0e423ff8db0bace77a5749494dd0c29bd4a6ca8e
Pragya45 <pphuyal170@gmail.com>
2024-06-04 13:20:18 +0545

    final onboarding

--- lib/features/onboarding/onboarding_view.dart
+++ lib/features/onboarding/onboarding_view.dart
@@ -6,76 +6,98 @@ class OnboardingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        actions: <Widget>[
+        actions: [
           TextButton(
             onPressed: () {
-              // Navigate to another screen or dismiss the onboarding
+              // Logic to skip the onboarding
             },
-            child: const Text('Skip', style: TextStyle(color: Colors.orange)),
+            child: const Text('Skip',
+                style: TextStyle(color: Colors.orange, fontSize: 18)),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/images/logo.png'), // Your app's logo
-            const SizedBox(height: 20),
-            const Text(
-              'Welcome to Meal Map',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 40), // Space from top
+              const Text(
+                'Welcome to Meal Map',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'your guide to the best dining experiences!',
-              style: TextStyle(fontSize: 18, color: Colors.grey),
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton.icon(
-              icon: const Icon(Icons.g_translate, color: Colors.white),
-              label: const Text('Continue with Google'),
-              onPressed: () {
-                // Logic to handle Google sign-in
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.orange,
-                minimumSize: const Size(double.infinity, 50), // full width
+              const SizedBox(height: 5),
+              const Text(
+                'your guide to the best dining experiences!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, color: Colors.grey),
               ),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to login screen
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Colors.white, // Text color
-                minimumSize: const Size(double.infinity, 50), // full width
-                side: const BorderSide(color: Colors.black), // Border color
+              Expanded(
+                child: Center(
+                  child: Image.asset('assets/images/logo.png',
+                      height: 200), // Larger logo
+                ),
               ),
-              child: const Text('Log in'),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to registration screen
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Colors.white, // Text color
-                minimumSize: const Size(double.infinity, 50), // full width
-                side: const BorderSide(color: Colors.black), // Border color
+              ElevatedButton.icon(
+                icon: Image.asset('assets/images/google_icon.png',
+                    height: 24, width: 24), // Google logo PNG
+                label: const Text('Continue with Google'),
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.orange,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                ),
               ),
-              child: const Text('I\'m new'),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        minimumSize: const Size.fromHeight(50),
+                        side: const BorderSide(color: Colors.black),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                      ),
+                      child: const Text('Log in'),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        minimumSize: const Size.fromHeight(50),
+                        side: const BorderSide(color: Colors.black),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                      ),
+                      child: const Text("I'm new"),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40), // Space from bottom
+            ],
+          ),
         ),
       ),
     );
