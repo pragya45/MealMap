218bc01ce7d3aa443583b868c55dad7a17ca85d2
Pragya45 <pphuyal170@gmail.com>
2024-06-16 15:25:27 +0545

    login complete

--- lib/features/login/login_view.dart
+++ lib/features/login/login_view.dart
@@ -1,72 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:mealmap/features/navbar/custom_bottom_nav_bar.dart';
+import 'package:mealmap/features/navbar/custom_top_nav_bar.dart';
+import 'package:mealmap/features/register/widgets/custom_textfield_view.dart';
 
-class CustomTextField extends StatefulWidget {
-  final String hintText;
-  final bool obscureText;
-  final String iconPath;
-
-  const CustomTextField({
-    Key? key,
-    required this.hintText,
-    this.obscureText = false,
-    required this.iconPath,
-  }) : super(key: key);
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _CustomTextFieldState createState() => _CustomTextFieldState();
-}
-
-class _CustomTextFieldState extends State<CustomTextField> {
-  late bool _obscureText;
-
-  @override
-  void initState() {
-    super.initState();
-    _obscureText = widget.obscureText;
-  }
-
-  void _toggleVisibility() {
-    setState(() {
-      _obscureText = !_obscureText;
-    });
-  }
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: TextField(
-        obscureText: _obscureText,
-        decoration: InputDecoration(
-          prefixIcon: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Image.asset(widget.iconPath, height: 14, width: 14),
-          ),
-          suffixIcon: widget.obscureText
-              ? GestureDetector(
-                  onTap: _toggleVisibility,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.asset(
-                      _obscureText
-                          ? 'assets/icons/eye_off.png'
-                          : 'assets/icons/eye.png',
-                      height: 16,
-                      width: 16,
+    return Scaffold(
+      appBar: const CustomTopNavBar(title: 'Meal Map'),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const CustomTextField(
+                hintText: 'Email',
+                iconPath: 'assets/icons/email.png',
+              ),
+              const CustomTextField(
+                hintText: 'Password',
+                iconPath: 'assets/icons/password.png',
+                obscureText: true,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    // Add your onPressed code here!
+                  },
+                  child: const Text(
+                    'Forgot Your Password?',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home'); // N
+                  // Handle login logic here
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF29912).withOpacity(0.8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 60,
+                    vertical: 15,
+                  ),
+                  minimumSize: const Size(190, 48), // Adjust button size
+                ),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account? "),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(
+                          context, '/register'); // Navigate to register page
+                    },
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 6, 130, 232),
+                        decoration: TextDecoration.underline,
+                      ),
                     ),
                   ),
-                )
-              : null,
-          hintText: widget.hintText,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: const BorderSide(
-              color: Colors.black,
-            ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                icon: Image.asset('assets/icons/google_icons.png', height: 24),
+                label: const Text(
+                  'Login with Google',
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  side: const BorderSide(color: Colors.black),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 90,
+                    vertical: 12,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                icon: Image.asset('assets/icons/facebook_icon.png', height: 24),
+                label: const Text(
+                  'Login with facebook',
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  side: const BorderSide(color: Colors.black),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 90,
+                    vertical: 12,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
+      bottomNavigationBar: const CustomBottomNavBar(),
     );
   }
 }
