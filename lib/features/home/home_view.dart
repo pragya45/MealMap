3ab061b078c839d3d29a75a02014145144937b09
Pragya45 <pphuyal170@gmail.com>
2024-06-27 11:39:43 +0545

    homepage completely fetched

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -8,6 +8,7 @@ class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _HomeViewState createState() => _HomeViewState();
 }
 
@@ -40,28 +41,40 @@ class _HomeViewState extends State<HomeView> {
   void _performSearch(String query) {
     setState(() {
       _isSearching = true;
-      _searchText = query;
-      if (_searchText.isEmpty) {
-        _filteredCategories = _categories;
-      } else {
-        _filteredCategories = _categories
-            .where((category) => category['name']
-                .toLowerCase()
-                .contains(_searchText.toLowerCase()))
-            .toList();
-        if (_filteredCategories.isNotEmpty) {
-          final matchedCategory = _filteredCategories.firstWhere(
-              (category) => category['name']
+    });
+
+    Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        _searchText = query;
+        if (_searchText.isEmpty) {
+          _filteredCategories = _categories;
+        } else {
+          _filteredCategories = _categories
+              .where((category) => category['name']
                   .toLowerCase()
-                  .contains(_searchText.toLowerCase()),
-              orElse: () => null);
-          if (matchedCategory != null) {
-            _filteredCategories.remove(matchedCategory);
-            _filteredCategories.insert(0, matchedCategory);
+                  .contains(_searchText.toLowerCase()))
+              .toList();
+          if (_filteredCategories.isNotEmpty) {
+            final matchedCategory = _filteredCategories.firstWhere(
+                (category) => category['name']
+                    .toLowerCase()
+                    .contains(_searchText.toLowerCase()),
+                orElse: () => null);
+            if (matchedCategory != null) {
+              _filteredCategories.remove(matchedCategory);
+              _filteredCategories.insert(0, matchedCategory);
+            }
           }
         }
-      }
-      _isSearching = false;
+        _isSearching = false;
+      });
+    });
+  }
+
+  void _refreshPage() {
+    setState(() {
+      _searchText = '';
+      _filteredCategories = _categories;
     });
   }
 
@@ -75,84 +88,90 @@ class _HomeViewState extends State<HomeView> {
           elevation: 0,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 20), // Add space above the banner
-            Center(
-              child: Image.asset(
-                'assets/images/banner.png',
-                fit: BoxFit.cover,
-                width: MediaQuery.of(context).size.width * 0.9,
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _refreshPage();
+        },
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 20), // Add space above the banner
+              Center(
+                child: Image.asset(
+                  'assets/images/banner.png',
+                  fit: BoxFit.cover,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            CustomSearchBar(onSearch: _performSearch),
-            FutureBuilder<List<dynamic>>(
-              future: _categoriesFuture,
-              builder: (context, snapshot) {
-                if (_isSearching) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.connectionState ==
-                    ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text('No categories found'));
-                } else {
-                  return GridView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 1,
-                      mainAxisSpacing: 1,
-                      childAspectRatio: 3 / 4,
-                    ),
-                    itemCount: _filteredCategories.length,
-                    itemBuilder: (context, index) {
-                      final category = _filteredCategories[index];
-                      return Stack(
-                        children: [
-                          CategoryTile(
-                            title: category['name'],
-                            imagePath:
-                                'assets/icons/${category['name'].toLowerCase()}.png',
-                            onTap: () {},
-                            highlight: category['name']
-                                .toLowerCase()
-                                .contains(_searchText.toLowerCase()),
-                          ),
-                          if (index < 3)
-                            const Positioned(
-                              bottom: 0,
-                              left: 0,
-                              right: 0,
-                              child: Divider(
-                                color: Colors.black,
-                                height: 1,
-                              ),
+              const SizedBox(height: 10),
+              CustomSearchBar(onSearch: _performSearch),
+              FutureBuilder<List<dynamic>>(
+                future: _categoriesFuture,
+                builder: (context, snapshot) {
+                  if (_isSearching) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text('No categories found'));
+                  } else {
+                    return GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 1,
+                        mainAxisSpacing: 1,
+                        childAspectRatio: 3 / 4,
+                      ),
+                      itemCount: _filteredCategories.length,
+                      itemBuilder: (context, index) {
+                        final category = _filteredCategories[index];
+                        return Stack(
+                          children: [
+                            CategoryTile(
+                              title: category['name'],
+                              imagePath:
+                                  'assets/icons/${category['name'].toLowerCase()}.png',
+                              onTap: () {},
+                              highlight: category['name']
+                                  .toLowerCase()
+                                  .contains(_searchText.toLowerCase()),
                             ),
-                          if (index % 3 != 2)
-                            const Positioned(
-                              top: 0,
-                              bottom: 0,
-                              right: 0,
-                              child: VerticalDivider(
-                                color: Colors.black,
-                                width: 1,
+                            if (index < 3)
+                              const Positioned(
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                child: Divider(
+                                  color: Colors.black,
+                                  height: 1,
+                                ),
                               ),
-                            ),
-                        ],
-                      );
-                    },
-                  );
-                }
-              },
-            ),
-          ],
+                            if (index % 3 != 2)
+                              const Positioned(
+                                top: 0,
+                                bottom: 0,
+                                right: 0,
+                                child: VerticalDivider(
+                                  color: Colors.black,
+                                  width: 1,
+                                ),
+                              ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: const CustomBottomNavBar(),
