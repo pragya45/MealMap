17b3939e3fc85b22f8e7b16be2486720cd81d128
Pragya45 <pphuyal170@gmail.com>
2024-07-11 01:14:23 +0545

    menu item

--- lib/features/detail/menu_view.dart
+++ lib/features/detail/menu_view.dart
@@ -10,7 +10,7 @@ class MenuView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Menu'),
+        title: const Text('Menu List'),
         backgroundColor:
             const Color.fromARGB(255, 255, 153, 0).withOpacity(0.8),
       ),
@@ -25,17 +25,115 @@ class MenuView extends StatelessWidget {
             return const Center(child: Text('No menu items found'));
           } else {
             final items = snapshot.data!;
-            return ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                final item = items[index];
-                return ListTile(
-                  leading: Image.network(item['image'],
-                      width: 50, height: 50, fit: BoxFit.cover),
-                  title: Text(item['name']),
-                  subtitle: Text('\$${item['price']}'),
-                );
-              },
+            return Column(
+              children: [
+                const Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          'ITEMS',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          'PRICE',
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      final item = items[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 9.0, horizontal: 10.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  child: Image.network(
+                                    item['image'],
+                                    width: 70,
+                                    height: 70,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  item['name'],
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(width: 20),
+                            Expanded(
+                              flex: 2,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    item['description'] ?? '',
+                                    style: const TextStyle(
+                                      color: Color.fromARGB(255, 0, 0, 0),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(
+                                'Rs.${item['price']}',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textAlign: TextAlign.right,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                if (items.length > 5)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        // Handle view all action
+                      },
+                      child: const Text(
+                        'View All',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+              ],
             );
           }
         },
